cc89de617fd1c7f87e9e9bc32ae17a24f87a1f8d
Stefan Valkov <s.3.valkov@gmail.com>
2023-12-17 18:36:32 +0200

    Initial commit

---
+++ src/main/java/com/s3valkov/vendingmachineapp/VendingMachineAppApplication.java
@@ -0,0 +1,13 @@
+package com.s3valkov.vendingmachineapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendingMachineAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendingMachineAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/s3valkov/vendingmachineapp/VendingMachineAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s3valkov.vendingmachineapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendingMachineAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
